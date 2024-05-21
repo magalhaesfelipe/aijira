a23c6df9680bad0a50f278d37149101ae174455f
Felipe Lopes <felipemlopes0009@gmail.com>
2024-05-20 23:51:37 -0400

    Cyber-Ghost

---
+++ cyber-ghost/src/App.js
@@ -0,0 +1,39 @@
+import "./styles.css";
+import image1 from "./images/img1.jpg";
+import image2 from "./images/img2.jpg";
+import image3 from "./images/img3.jpg";
+import image4 from "./images/img4.jpg";
+import image5 from "./images/img5.jpg";
+import image6 from "./images/img6.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">
+        <h1>Hyper Cube</h1>
+      </div>
+      <div className="grid-wrapper">
+        <div>
+          <img src={image1} />
+        </div>
+        <div>
+          <img src={image2} />
+        </div>
+        <div>
+          <img src={image3} />
+        </div>
+        <div>
+          <img src={image4} />
+        </div>
+        <div>
+          <img src={image5} />
+        </div>
+        <div>
+          <img src={image6} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
