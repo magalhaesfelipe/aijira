a8af707096a89a091921de560dd68a49afb0c9d7
zer0 <ffelipemagallhaes@gmail.com>
2025-09-28 02:14:30 -0400

    feat: render images dynamically

--- cyber-ghost/src/pages/home/HomePage.tsx
+++ cyber-ghost/src/pages/home/HomePage.tsx
@@ -1,34 +1,20 @@
 import s from "./homepage.module.css";
 
-import img1 from "../../assets/images/img1.jpg";
-import img2 from "../../assets/images/img2.jpg";
-import img3 from "../../assets/images/img3.jpg";
-import img4 from "../../assets/images/img4.jpg";
-import img5 from "../../assets/images/img5.jpg";
-import img6 from "../../assets/images/img6.jpg";
+const imageModules = import.meta.glob(
+  "../../assets/images/*.{jpg,jpeg,png,webp,svg}",
+  { eager: true }
+);
+const images = Object.values(imageModules).map((mod) => mod.default);
 
 function HomePage() {
   return (
     <div className={s.sc}>
       <div className={s.grid}>
-        <div className={s.block}>
-          <img src={img1} />
-        </div>
-        <div className={s.block}>
-          <img src={img2} />
-        </div>
-        <div className={s.block}>
-          <img src={img3} />
-        </div>
-        <div className={s.block}>
-          <img src={img4} />
-        </div>
-        <div className={s.block}>
-          <img src={img5} />
-        </div>
-        <div className={s.block}>
-          <img src={img6} />
-        </div>
+        {images.map((src, index) => (
+          <div key={index} className={s.block}>
+            <img src={src} alt={`image-${index}`} />
+          </div>
+        ))}
       </div>
     </div>
   );
