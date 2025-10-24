4fee051550f40f9982bf701c74317a2412c2b92e
felipe.magalhaes <ffelipemagallhaes@gmail.com>
2025-10-23 23:46:01 -0400

    styles: add auto horizontal scrolling

--- cyber-ghost/src/components/layout/header/Header.tsx
+++ cyber-ghost/src/components/layout/header/Header.tsx
@@ -2,11 +2,34 @@ import s from "./header.module.css";
 import icon from "../../../assets/icons/nearby_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
 
 const Header = () => {
+  const items = [
+    "A CYBERPUNK COLLECTION",
+    "A CYBERPUNK COLLECTION",
+    "A CYBERPUNK COLLECTION",
+  ];
+
   return (
     <div className={s.sc}>
       <div className={s.title2}>
-        <h1>A CYBERPUNK COLLECTION</h1>
+        <div className={s.track}>
+          <div className={s.group}>
+            {items.map((t, i) => (
+              <h1 key={`a-${i}`}>{t}</h1>
+            ))}
+          </div>
+          <div className={s.group}>
+            {items.map((t, i) => (
+              <h1 key={`b-${i}`}>{t}</h1>
+            ))}
+          </div>
+          <div className={s.group}>
+            {items.map((t, i) => (
+              <h1 key={`c-${i}`}>{t}</h1>
+            ))}
+          </div>
+        </div>
       </div>
+
       <div className={s.imgContainer} title="About">
         <img src={icon} alt="Nearby Icon" />
       </div>
